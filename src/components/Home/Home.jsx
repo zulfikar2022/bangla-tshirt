88a0b797f8795f444e27f1b3ced4b656ceb7e28e
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-04-09 16:47:38 +0600

    first commit of bangla tshirt

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const tshirts = useLoaderData();
+    return (
+        <div>
+            <h2>This is home:  {tshirts.length}</h2>
+        </div>
+    );
+};
+
+export default Home;
